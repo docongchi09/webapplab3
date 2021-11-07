0a98f3c2e392eb72fc1c830190a64370cab33715
docongchi09 <17521292@gm.uit.edu.vn>
2021-11-07 19:27:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const bodyparse = require('body-parser');
+const express = require('express');
+const methodOverride = require('method-override');
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyparse.json({
+    limit:'5mb'
+}));
+app.use(bodyparse.json({
+    type:'application/vnd.api+json'
+}));
+app.use(bodyparse.urlencoded({
+    limit:'5mb',
+    extended:true
+}));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.get('/', (req, res) => res.send('API Running'));
+
+
+require('./routes')(app);
