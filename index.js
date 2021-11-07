9bac3857639713e7bc9e39637a74a59680e77da5
docongchi09 <91005496+docongchi09@users.noreply.github.com>
2021-11-07 19:50:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,5 +17,7 @@ app.use(bodyparse.urlencoded({
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.get('/', (req, res) => res.send('API Running'));
 
-
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 require('./routes')(app);
